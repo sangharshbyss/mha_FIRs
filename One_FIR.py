5aeb359d2d4420cabb450784b4f23bdb0fd548e1
sangharshmanuski <sangharshbyss@gmail.com>
2020-06-24 12:09:51 +0530

    FIR_logging and One_FIR: two code files uploaded

---
+++ One_FIR.py
@@ -0,0 +1,121 @@
+"""This file is not fully ready, coz, selection act is not going well."""
+import base64
+from typing import List
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import os
+from PIL import Image
+from io import BytesIO
+
+from pytesseract import pytesseract
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, \
+    ElementNotInteractableException, WebDriverException
+
+from FIR_logging import logger
+
+# constants
+URL = r'https://www.mhpolice.maharashtra.gov.in/Citizen/MH/PublishedFIRs.aspx'
+
+options = FirefoxOptions()
+# options.add_argument("--headless")
+options.add_argument("--private-window")
+driver = webdriver.Firefox(options=options)
+# base dir path for downloading files:
+Download_Directory = r'/home/sangharshmanuski/Documents/mha_FIRs/raw_footage'
+
+wait = WebDriverWait(driver, 180)
+# empty list to store name of police stations where records were not found
+record_not_found = []
+# empty list to store name of police stations where records were found
+record_found = []
+# three_months_back = 'three months back from yesterday'
+# yesterday = 'yesterday'
+# open the page
+driver.get(URL)
+logger.info('page open')
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                           '#ContentPlaceHolder1_txtDateOfRegistrationFrom')))
+driver.refresh()
+unit_list = Select(driver.find_element_by_css_selector('#ContentPlaceHolder1_ddlDistrict'))
+unit_names = [o.get_attribute(
+    "text") for o in unit_list.options if o.get_attribute("value") != 'Select']
+view_record = Select(driver.find_element_by_css_selector('#ContentPlaceHolder1_ucRecordView_ddlPageSize'))
+view_record_max = view_record.select_by_value('50')
+search = driver.find_element_by_css_selector('#ContentPlaceHolder1_btnSearch')
+# empty list
+act_column = []
+FIR_column = []
+
+# enter dates
+
+def enter_start_date(three_months_back, yesterday):
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                               '#ContentPlaceHolder1_txtDateOfRegistrationFrom')))
+    datefield = driver.find_element_by_css_selector('#ContentPlaceHolder1_txtDateOfRegistrationFrom')
+
+    end_datefield = driver.find_element_by_css_selector('#ContentPlaceHolder1_txtDateOfRegistrationTo')
+
+    ActionChains(driver).click(datefield).send_keys(
+        three_months_back).move_to_element(end_datefield).click().send_keys(yesterday).perform()
+
+    logger.info('start date')
+
+
+
+def enter_unit(unit_number=0):
+    unit_list = Select(driver.find_element_by_css_selector('#ContentPlaceHolder1_ddlDistrict'))
+    unit_list.select_by_index(unit_number)
+    unit_name = unit_names[unit_number]
+    return unit_name
+
+
+def enter_police_station(number=0):
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#ContentPlaceHolder1_ddlPoliceStation')))
+    police_stations = Select(driver.find_element_by_css_selector('#ContentPlaceHolder1_ddlPoliceStation'))
+    police_stations_names = [police.get_attribute("text") for police
+                             in police_stations.options if police.get_attribute("value") != '']
+    police_stations.select_by_index(number)
+    police_stations_name = police_stations_names[number]
+    return police_stations_name
+
+
+def get_records():
+
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#ContentPlaceHolder1_lbltotalrecord')))
+
+    all_column = driver.find_elements_by_tag_name('td')
+
+    atrocity_act = "महाराष्ट्र दारूबंदी अधिनियम,१९४९ - 65(e)"
+    FIR_no = "*./"
+    for act in all_column:
+        if atrocity_act in act.text:
+            act_column.append(act.text)
+        '''if FIR_no in act.text:
+            FIR_column.append(FIR_no)'''
+    print(act_column)
+
+'''
+def list_poa_caes():
+    list_of_poa = []
+    for act, FIR_number in zip(act_column, FIR_column):
+        list_of_poa.append(FIR_number)
+    return print(list_of_poa)
+'''
+enter_start_date("01042020", "21062020")
+
+
+enter_unit(2)
+time.sleep(5)
+enter_police_station(4)
+driver.find_element_by_css_selector('#ContentPlaceHolder1_btnSearch').click()
+get_records()
+
+
+
