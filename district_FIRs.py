b47226206182d23637bb3c4cdc0d4a3d46a396ca
sangharshmanuski <sangharshbyss@gmail.com>
2020-06-26 06:27:27 +0530

    data to store at level of state

--- district_FIRs.py
+++ district_FIRs.py
@@ -18,6 +18,7 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 
 
+
 # constants
 URL = r'https://www.mhpolice.maharashtra.gov.in/Citizen/MH/PublishedFIRs.aspx'
 
@@ -42,7 +43,8 @@ logger.info('page open')
 
 # empty list
 
-dataframes= []
+dataframes = []
+cy_data = []
 COLUMNS = ['Sr.No.', 'State', 'District', 'Police Station', 'Year', 'FIR No.', 'Registration Date', 'FIR No', 'Sections']
 
 # enter dates
@@ -64,18 +66,25 @@ def get_records(driver):
     # first extract the data from current page.
     soup = BS(driver.page_source, 'html.parser')
     main_table = soup.find("table", {"id": "ContentPlaceHolder1_gdvDeadBody"})
+    if not len(main_table) > 0:
+        time.sleep(5)
+
     rows = main_table.find_all("tr")
 
-    cy_data = []
-    for row in rows[0:(len(rows)-2)]:
-        cells = row.find_all('td')
-        cells = cells[0:9]
-        cy_data.append([cell.text for cell in cells])
-    dataframes.append(pd.DataFrame(cy_data, columns=COLUMNS))
-    logger.info(dataframes)
+    if len(rows) > 0:
+
+        for row in rows[0:(len(rows)-2)]:
+            cells = row.find_all('td')
+            cells = cells[0:9]
+            cy_data.append([cell.text for cell in cells])
+
+
     # now iterate over each page and extract the data.
     # 1. get all the clickable links form bottom of the page
-    all_pages = driver.find_elements_by_css_selector('.gridPager a')
+    try:
+        all_pages = driver.find_elements_by_css_selector('.gridPager a')
+    except NoSuchElementException:
+        return
     # iterate over each page
     # range is selected for continuance.
     for each in range(len(all_pages)-1):
@@ -85,68 +94,64 @@ def get_records(driver):
         soup = BS(driver.page_source, 'html.parser')
         main_table = soup.find("table", {"id": "ContentPlaceHolder1_gdvDeadBody"})
         rows = main_table.find_all("tr")
+        if len(rows) > 0:
 
-        cy_data = []
-        for row in rows[0:(len(rows) - 2)]:
-            cells = row.find_all('td')
-            cells = cells[0:9]
-            cy_data.append([cell.text for cell in cells])
-        dataframes.append(pd.DataFrame(cy_data, columns=COLUMNS))
-        logger.info(dataframes)
-
-    return dataframes
+            for row in rows[0:(len(rows) - 2)]:
+                cells = row.find_all('td')
+                cells = cells[0:9]
+                cy_data.append([cell.text for cell in cells])
 
 
 
-
-
-
-
-counter = 1
+counter = 25
 
 while counter < 49:
-    options = FirefoxOptions()
-    # options.add_argument("--headless")
-    options.add_argument("--private-window")
-    driver = webdriver.Firefox(options=options)
-    driver.get(URL)
-    driver.refresh()
-    time.sleep(3)
-    view = Select(driver.find_element_by_css_selector(
-        '#ContentPlaceHolder1_ucRecordView_ddlPageSize'))
-    view.select_by_value('50')
-    enter_date(driver=driver, three_months_back='19062020', yesterday='23062020')
-    unit_list = Select(driver.find_element_by_css_selector("#ContentPlaceHolder1_ddlDistrict"))
-    unit_names = [o.get_attribute("text")
-                  for o in unit_list.options]
-    unit_values = [o.get_attribute("text")
-                   for o in unit_list.options if o.get_attribute("value") != 'Select']
-    unit_list.select_by_index(counter)
-    time.sleep(2)
-
-    driver.find_element_by_css_selector('#ContentPlaceHolder1_btnSearch').click()
-    time.sleep(10)
-    number_of_records_found = driver.find_element_by_css_selector(
-        '#ContentPlaceHolder1_lbltotalrecord').text
     try:
+        options = FirefoxOptions()
+        # options.add_argument("--headless")
+        options.add_argument("--private-window")
+        driver = webdriver.Firefox(options=options)
+        driver.get(URL)
+        driver.refresh()
+        time.sleep(3)
+        view = Select(driver.find_element_by_css_selector(
+            '#ContentPlaceHolder1_ucRecordView_ddlPageSize'))
+        view.select_by_value('50')
+        enter_date(driver=driver, three_months_back='19062020', yesterday='23062020')
+        unit_list = Select(driver.find_element_by_css_selector("#ContentPlaceHolder1_ddlDistrict"))
+        unit_names = [o.get_attribute("text")
+                      for o in unit_list.options]
+        unit_values = [o.get_attribute("text")
+                       for o in unit_list.options if o.get_attribute("value") != 'Select']
+        unit_list.select_by_index(counter)
+        time.sleep(2)
+
+        driver.find_element_by_css_selector('#ContentPlaceHolder1_btnSearch').click()
+        time.sleep(10)
+        number_of_records_found = driver.find_element_by_css_selector(
+            '#ContentPlaceHolder1_lbltotalrecord').text
+
+        try:
 
 
-        get_records(driver=driver)
-        record_found.append(unit_names[counter])
-        logger.info(f'{unit_names[counter]} no. of records: {number_of_records_found}')
+            get_records(driver=driver)
+            record_found.append(unit_names[counter])
+            logger.info(f'{unit_names[counter]} no. of records: {number_of_records_found}')
 
-    except TimeoutException:
-        record_not_found.append(unit_names[counter])
+        except TimeoutException:
+            record_not_found.append(unit_names[counter])
+            counter += 1
+            continue
+
         counter += 1
+        driver.close()
+        print(record_found, record_not_found)
+    except (NoSuchElementException, TimeoutException):
         continue
+dataframes.append(pd.DataFrame(cy_data, columns=COLUMNS))
+district_data = pd.concat(dataframes)
 
-    district_data = pd.concat(dataframes)
-
-    district_data.to_csv(os.path.join(Download_Directory, f'{unit_names[counter]}.csv'))
-    counter += 1
-    driver.close()
-    print(record_found, record_not_found)
-
+district_data.to_csv(os.path.join(Download_Directory, f'19_06_to_23_06.csv'))
 
 
 
