4187836ce4ad84b6b7e006a3a73dcb51a2f4acb7
sangharshmanuski <sangharshbyss@gmail.com>
2020-07-01 21:43:31 +0530

    modified page_slot_1.py to limit the rotation on pages - the function modified sligthly and now working fine.

--- page_slot_1.py
+++ page_slot_1.py
@@ -75,6 +75,20 @@ def number_of_records():
         return False
 
 
+def total_pages(district):
+    numbers = number_of_records()
+    logger.info(f"number of records @ {district}:"
+                f" {numbers}")
+    records_found.append(f'{district}:'
+                         f' {numbers}')
+    # deciding ideal number of pages on basis of number of records
+    ideal_total_pages = int(numbers) / 50
+    if type(ideal_total_pages) is float:
+        ideal_total_pages = int(numbers) // 50
+    logger.info(f'{ideal_total_pages}')
+    return ideal_total_pages
+
+
 def extract_table_current(single):
     # entire table of record to be taken to the list.
     soup = BS(driver.page_source, 'html.parser')
@@ -105,37 +119,33 @@ def extract_table_multipage(single):
         single.append([cell.text for cell in cells])
 
 
-def next_page(clicks):
+def next_page(clicks, data, district):
     # check if any link to next page is available
     # iterate every page.
+    ideal_pages = total_pages(district)
     try:
         link_for_page = driver.find_element_by_css_selector('.gridPager a')
     except NoSuchElementException:
         return False
     links_for_pages = driver.find_elements_by_css_selector('.gridPager a')
-    for page in range(len(links_for_pages)):
-        time.sleep(5)
-
-        # new list, to by pass stale element exception
-        links_for_pages_new = driver.find_elements_by_css_selector('.gridPager a')
-        # do not click on link for new page
-        if links_for_pages_new[page].text != '...':
-            click_it = links_for_pages_new[page].click()
-            clicks.append(click_it)
-            time.sleep(7)
-            logger.info(f'page {page}')
-            soup = BS(driver.page_source, 'html.parser')
-            main_table = soup.find("table", {"id": "ContentPlaceHolder1_gdvDeadBody"})
-
-            rows = main_table.find_all("tr")
-
-            for row in rows[0:(len(rows)) - 2]:
-                cells = row.find_all('td')
-                cells = cells[0:9]  # drop the last column
-
-                # store data in list
-                page_data.append([cell.text for cell in cells])
-
+    try:
+        for page in range(len(links_for_pages)):
+            if len(clicks) < ideal_pages:
+                # new list, to by pass stale element exception
+                links_for_pages_new = driver.find_elements_by_css_selector('.gridPager a')
+                # do not click on link for new page
+                if links_for_pages_new[page].text != '...':
+                    click_it = links_for_pages_new[page].click()
+                    clicks.append(click_it)
+                    time.sleep(7)
+                    logger.info(f'page {page}')
+                    extract_table_multipage(data)
+            else:
+                logger.info(f'ideal number of pages completed')
+                return
+    except TimeoutException:
+        logger.info(f'TimeOutException @ {district}')
+        return
 
 def second_page_slot():
     # find specific link for going to page 11 and click.
@@ -184,7 +194,8 @@ class Search:
         logger.info('date entered')
 
     def search_the_district(self):
-        dist_list = Select(driver.find_element_by_css_selector("#ContentPlaceHolder1_ddlDistrict"))
+        dist_list = Select(driver.find_element_by_css_selector(
+            "#ContentPlaceHolder1_ddlDistrict"))
         dist_names = [o.get_attribute("text")
                       for o in dist_list.options if o.get_attribute("value") != 'Select']
         dist_values = [o.get_attribute("value")
@@ -229,7 +240,7 @@ for unit in unit_values:
     driver.refresh()
     page_data = []
     district_dataframe = []
-    clicks = []
+    number_of_clicks = []
     wait_for_page_load(driver)
     view = Select(driver.find_element_by_css_selector(
         '#ContentPlaceHolder1_ucRecordView_ddlPageSize'))
@@ -251,26 +262,13 @@ for unit in unit_values:
         logger.info(f"some issues yet. skip @ {unit_names[unit_values.index(unit)]}")
 
         continue
-    else:
-        numbers = number_of_records()
-        logger.info(f"number of records @ {unit_names[unit_values.index(unit)]}:"
-                    f" {number_of_records()}")
-        records_found.append(f'{unit_names[unit_values.index(unit)]}:'
-                             f' {numbers}')
-        # deciding number of pages on basis of number of records
-        total_pages = int(numbers)/50
-        if type(total_pages) is not float:
-
-            total_pages = int(numbers)//50 + 1
-        logger.info(f'{total_pages}')
-
 
     extract_table_current(page_data)
     try:
-        next_page(clicks)
+        next_page(number_of_clicks, page_data, this.district_name)
     except:
         district_data = pd.DataFrame(page_data, columns=COLUMNS)
-        district_data.to_csv(os.path.join(Download_Directory, f'{unit_names[unit_values.index(unit)]}'
+        district_data.to_csv(os.path.join(Download_Directory, f'{this.district_name}'
                                                               f'_15_06_to_25_06.csv'))
         driver.close()
         continue
@@ -279,25 +277,24 @@ for unit in unit_values:
 
         link_for_page_slot = driver.find_element_by_link_text('...')
         link_for_page_slot.click()
-        clicks.append(clicks)
+        number_of_clicks.append(number_of_clicks)
         extract_table_multipage(page_data)
-        next_page(clicks)
+        next_page(number_of_clicks, page_data, this.district_name)
     except:
         district_data = pd.DataFrame(page_data, columns=COLUMNS)
-        district_data.to_csv(os.path.join(Download_Directory, f'{unit_names[unit_values.index(unit)]}'
+        district_data.to_csv(os.path.join(Download_Directory, f'{this.district_name}'
                                                               f'_15_06_to_25_06.csv'))
-
         continue
     try:
         next_page_slot = driver.find_element_by_css_selector(
             '.gridPager > td:nth-child(1) > table:nth-child'
             '(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(12) > a:nth-child(1)')
         next_page_slot.click()
-        clicks.append(clicks)
+        number_of_clicks.append(number_of_clicks)
         extract_table_multipage(page_data)
-        next_page(clicks)
+        next_page(number_of_clicks, page_data, this.district_name)
         district_data = pd.DataFrame(page_data, columns=COLUMNS)
-        district_data.to_csv(os.path.join(Download_Directory, f'{unit_names[unit_values.index(unit)]}'
+        district_data.to_csv(os.path.join(Download_Directory, f'{this.district_name}'
                                                               f'_15_06_to_25_06.csv'))
         driver.close()
     except:
